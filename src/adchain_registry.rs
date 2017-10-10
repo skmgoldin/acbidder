bcf6bc0c75db35c724e2e4b20e51edfbfbf9293c
Mike Goldin <mike.goldin@consensys.net>
2017-10-10 12:37:03 +0200

    Improve adchain_registry, see commit body
    * Parameterize the address of the registry to a const in RegistryInstance::new
    * Return the result of is_in_registry

--- src/adchain_registry.rs
+++ src/adchain_registry.rs
@@ -11,23 +11,26 @@ pub struct RegistryInstance<'a, T: 'a + web3::Transport> {
 }
 
 impl<'a, T: web3::Transport> RegistryInstance<'a, T> {
+
     pub fn new(web3: &'a web3::Web3<T>) -> RegistryInstance<'a, T> {
+        const REGISTRY_ADDR: &str = "0x9452b742302e50a28660fd00748fa05e071f1470";
+
         let instance = Contract::from_json(
             web3.eth(),
-            H160::from_str("0x1a5cdcFBA600e0c669795e0B65c344D5A37a4d5A").unwrap(),
+            H160::from_str(REGISTRY_ADDR).unwrap(), //TODO:Make static
             include_bytes!("../Registry.json")
         ).unwrap();
 
         RegistryInstance {
-            instance 
+            instance
         }
     }
 
     pub fn is_in_registry(&self, domain: &str) -> bool {
         let domain = String::from(domain).into_token();
-        let result: bool = self.instance.query("listingMap", (domain, ), None,
+        let result: bool = self.instance.query("isWhitelisted", (domain, ), None,
             web3::contract::Options::default(), BlockNumber::Latest).wait().unwrap();
-        true
+        result
     }
 }
 
